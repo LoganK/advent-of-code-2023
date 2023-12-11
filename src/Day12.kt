248298eb0254b8dc17639dfd4b1dce67495c4f49
Logan Kennelly <logank@logank.dev>
2023-12-11 09:25:25 -0800

    Day 12: Part 2 complete
    Switch to a recursive solution for simplicity and efficiency. Even
    bulk-skipping '.' characters, it was still far too slow. Memoize
    the results and it's pretty darn fast (if awkward in Kotlin).
    
    Fixed the incorrect parsing for part 2 (missing added '?').
    
    Switch group lengths to Int to avoid casting at all indexing points.

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,50 +1,75 @@
 import java.io.File
 
-data class Spring(val record: String, val groups: List<Long>) {
+private class MemoizedCombinations {
+  val memoize = mutableMapOf<Pair<String, List<Int>>, Long>()
+
+  fun combinations(record: String, groups: List<Int>): Long =
+      memoize.getOrPut(Pair(record, groups), { _combinations(record, groups) })
+
+  // Support for `combinations()` to support recursion.
+  private fun _combinations(record: String, groups: List<Int>): Long {
+    if (groups.isEmpty()) {
+      // The current combination is valid depending on whether or not we have more springs.
+      return if (record.indexOf('#') >= 0) 0L else 1L
+    }
+
+    val slimRec = record.trim('.')
+    if (slimRec.length == 0) {
+      // No more combinations to be had.
+      return 0L
+    }
+
+    val nextGroupLen = groups[0]
+    val nextChar = slimRec.getOrNull(nextGroupLen) ?: '.'
+    return when (slimRec[0]) {
+      '?' ->
+          combinations('#' + slimRec.drop(1), groups) + combinations('.' + slimRec.drop(1), groups)
+      '#' ->
+          if (slimRec.length < nextGroupLen || slimRec.take(nextGroupLen).any { it == '.' }) {
+            // Current group too short to start here.
+            0L
+          } else if (nextChar == '#') {
+            // Current group too long to start here.
+            0L
+          } else if (nextChar == '?') {
+            // Need it to not terminate.
+            combinations(slimRec.drop(nextGroupLen + 1), groups.drop(1))
+          } else {
+            combinations(slimRec.drop(nextGroupLen), groups.drop(1))
+          }
+      else -> throw IllegalStateException("Unknown char '${slimRec[0]}'")
+    }
+  }
+}
+
+data class Spring(val record: String, val groups: List<Int>) {
   companion object {
     fun fromString(input: String): Spring {
       val (mapStr, groupStr) = input.split(' ', limit = 2)
-      return Spring(mapStr, groupStr.split(',').map(String::toLong))
+      return Spring(mapStr, groupStr.split(',').map(String::toInt))
     }
 
     fun fromString(input: List<String>): List<Spring> = input.map(Spring::fromString)
-  }
 
-  fun combinations(): Sequence<Spring> = sequence {
-    // Brute force all combinations.
-    val questions = record.filter { it == '?' }.count()
-    assert(questions < 64)
-    for (guess in (0 ..< (1L shl questions))) {
-      val fill = guess.toString(radix = 2).reversed().toMutableList()
-      yield(
-          Spring(
-              record
-                  .map {
-                    when (it) {
-                      '?' ->
-                          when (fill.removeFirstOrNull() ?: '0') {
-                            '0' -> '.'
-                            else -> '#'
-                          }
-                      else -> it
-                    }
-                  }
-                  .joinToString(separator = ""),
-              groups))
+    fun fromString2(input: String): Spring {
+      val (mapStr, groupStr) = input.split(' ', limit = 2)
+      val mapStr2 = (1..5).map { mapStr }.joinToString(separator = "?")
+      val groupStr2 = (1..5).map { groupStr }.joinToString(separator = ",")
+      return Spring(mapStr2, groupStr2.split(',').map(String::toInt))
     }
+
+    fun fromString2(input: List<String>): List<Spring> = input.map(Spring::fromString2)
   }
 
-  fun calcGroups(): List<Long> =
-      record.split('.').filterNot(String::isEmpty).map { it.length.toLong() }
-
-  fun valid(): Boolean = calcGroups() == groups
+  fun combinations(): Long {
+    return MemoizedCombinations().combinations(record, groups)
+  }
 }
 
 fun main() {
-  fun part1(input: List<Spring>): Long =
-      input.map { it.combinations().filter(Spring::valid).count().toLong() }.sum()
+  fun part1(input: List<Spring>): Long = input.map { it.combinations() }.sum()
 
-  fun part2(input: List<Spring>): Long = 1L
+  fun part2(input: List<Spring>): Long = input.map { it.combinations() }.sum()
 
   val testStr =
       """
@@ -65,6 +90,9 @@ fun main() {
   println(part1(test))
   println(part1(input))
 
-  println(part2(test))
-  println(part2(input))
+  val test2 = Spring.fromString2(testStr)
+  val input2 = Spring.fromString2(inputStr)
+
+  println(part2(test2))
+  println(part2(input2))
 }
