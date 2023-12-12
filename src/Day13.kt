62abb5612147511b76e0a23915e4b8db88944d7e
Logan Kennelly <logank@logank.dev>
2023-12-12 05:49:31 -0800

    Day 13: Part 2 w/ incorrect answer
    Going to redesign for simplicity now that I know part 2.

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,4 +1,9 @@
 import java.io.File
+import java.math.BigInteger
+
+fun <T> Collection<T>.pairCombinations(): Sequence<Pair<T, T>> = sequence {
+  forEachIndexed { i, a -> drop(i + 1).forEach { b -> yield(Pair(a, b)) } }
+}
 
 data class Room(val rows: Map<String, List<Int>>, val cols: Map<String, List<Int>>) {
   companion object {
@@ -16,11 +21,11 @@ data class Room(val rows: Map<String, List<Int>>, val cols: Map<String, List<Int
     }
 
     fun fromString(input: String): List<Room> {
-      return input.split("\n\n").map { parseRoom(it.lines()) }
+      return input.split("\n\n").map { parseRoom(it.lines().filter(String::isNotEmpty)) }
     }
   }
 
-  fun findSymmetry(input: Map<String, List<Int>>): Int {
+  fun <T> findSymmetry(input: Map<T, List<Int>>): Int {
     // Anything where two identical strings are adjacent.
     val candidates =
         input.flatMap { entry ->
@@ -40,11 +45,39 @@ data class Room(val rows: Map<String, List<Int>>, val cols: Map<String, List<Int
   }
 
   fun calcScore(): Long = (findSymmetry(cols) + 100 * findSymmetry(rows)).toLong()
+
+  fun findSmudgeSymmetry(input: Map<String, List<Int>>): Int {
+    // Use bits for convenient XOR.
+    fun String.toRoomNum(): BigInteger =
+        BigInteger(map { if (it == '#') '1' else '0' }.joinToString(separator = ""), 2)
+    fun BigInteger.singleBit(): Boolean = (this and (this - BigInteger.ONE)) == BigInteger.ZERO
+    val newInput = input.map { it.key.toRoomNum() to it.value }.toMap()
+
+    // Anything where almost identical strings are adjacent.
+    val candidateKeys =
+        newInput.keys.pairCombinations().filter { (it.first xor it.second).singleBit() }
+    for (candidate in candidateKeys) {
+      // There could be multiple matches, but we can swap only one candidate.
+      for (index in newInput[candidate.second]!!) {
+        var remap = newInput.toMutableMap()
+        remap[candidate.second] = remap[candidate.second]!!.minusElement(index)
+        remap[candidate.first] = remap[candidate.first]!!.plusElement(index)
+        val n = findSymmetry(remap)
+        if (n > 0) {
+          return n
+        }
+      }
+    }
+
+    return 0
+  }
+
+  fun calcScore2(): Long = (100 * findSmudgeSymmetry(rows) + findSmudgeSymmetry(cols)).toLong()
 }
 
 fun main() {
   fun part1(input: List<Room>): Long = input.map(Room::calcScore).sum()
-  fun part2(input: List<Room>): Long = 1L
+  fun part2(input: List<Room>): Long = input.map(Room::calcScore2).sum()
 
   val testStr =
       """
